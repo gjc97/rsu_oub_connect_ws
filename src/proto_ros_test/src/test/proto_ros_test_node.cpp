2ea1a03ceb3c7c58b1a16ee1055de20143360392
gjc97 <1084129576@qq.com>
2024-02-27 23:36:45 +0800

    none

--- src/proto_ros_test/src/test/proto_ros_test_node.cpp
+++ src/proto_ros_test/src/test/proto_ros_test_node.cpp
@@ -151,7 +151,7 @@ int main(int argc, char **argv)
 
         //***********************************反序列化测试****************************************
 
-        if (0)
+        if (1)
         {
 
             int nDataLen = 0;
@@ -173,5 +173,33 @@ int main(int argc, char **argv)
         // closesocket(sockClient);
         close(udp_rsu->sock_fd);
         return 0;
+
+        std::string people_str;
+        {
+            // 对一个联系人的信息使用PB进行序列化，并将结果打印出来
+            contacts::PeopleInfo people;
+            people.set_name("fl");
+            people.set_age(22);
+            if (!people.SerializeToString(&people_str))
+            {
+                std::cerr << "序列化联系人失败" << std::endl;
+                return -1;
+            }
+            std::cout << "序列化成功，结果：" << people_str << std::endl;
+        }
+
+        {
+            // 对序列化后的内容使用PB进行反序列化，解析出联系人信息并打印出来
+            contacts::PeopleInfo people;
+            if (!people.ParseFromString(people_str))
+            {
+                std::cerr << "反序列化联系人失败" << std::endl;
+                return -1;
+            }
+            std::cout << "反序列化成功！" << people_str << std::endl
+                      << "姓名： " << people.name() << std::endl
+                      << "年龄： " << people.age() << std::endl;
+        }
+        // return 0;
     }
 }
